d08524c9a729660f2a3858d1b3e5becc5f080231
YugaWatanabe <yuga.watanabe@wata-lab.meijo-u.ac.jp>
2019-11-01 04:21:52 +0900

    Fix: リンクが張られていないのを修正

--- lcom_web/join/check.php
+++ lcom_web/join/check.php
@@ -35,6 +35,19 @@ if(!empty($_POST)){
     <title>確認画面</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">メニュー</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+	  <a class="nav-item nav-link" href="../index.php">ホーム</a>
+	  <a class="nav-item nav-link" href="../project/joinproject.php">プロジェクトを探す</a>
+      <a class="nav-item nav-link active" href="#">ログイン<span class="sr-only">(current)</span></a>
+    </div>
+  </div>
+</nav>
 
 <div class="container">
     <form action="" method="post">
@@ -58,7 +71,7 @@ if(!empty($_POST)){
 				<img src="../image/icon/<?php print(htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES)); ?>">
             <?php endif; ?>
         </dd>
-        <div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
+        <div><a href="join.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
 	</dl>
 </div>
 
